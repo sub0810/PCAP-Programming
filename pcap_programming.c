62a79e35e1d04bb3883f5867f037d7bbd6f62c66
sub0810 <limhanseop0810@gmail.com>
2025-03-16 20:13:35 +0900

    fin

--- pcap_programming.c
+++ pcap_programming.c
@@ -27,33 +27,51 @@ struct ipheader {
   struct  in_addr    iph_destip;   //Destination IP address
 };
 
+/* TCP Header */
+struct tcpheader {
+  u_short tcp_sport;               /* source port */
+  u_short tcp_dport;               /* destination port */
+  u_int   tcp_seq;                 /* sequence number */
+  u_int   tcp_ack;                 /* acknowledgement number */
+  u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
+  u_char  tcp_flags;
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+  u_short tcp_win;                 /* window */
+  u_short tcp_sum;                 /* checksum */
+  u_short tcp_urp;                 /* urgent pointer */
+};
+
+void print_mac(u_char host[6]){
+  printf("%02X:%02X:%02X:%02X:%02X:%02X\n",
+          host[0], host[1], host[2], host[3], host[4], host[5]);
+}
+
 void got_packet(u_char *args, const struct pcap_pkthdr *header,
                               const u_char *packet)
 {
   struct ethheader *eth = (struct ethheader *)packet;
+  printf("eth    From: "); print_mac(eth->ether_shost);   
+  printf("eth      To: "); print_mac(eth->ether_dhost); 
 
   if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
     struct ipheader * ip = (struct ipheader *)
                            (packet + sizeof(struct ethheader)); 
+    printf("ip     From: %s\n", inet_ntoa(ip->iph_sourceip));   
+    printf("ip       To: %s\n", inet_ntoa(ip->iph_destip));    
 
-    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
-    printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
-
-    /* determine protocol */
-    switch(ip->iph_protocol) {                                 
-        case IPPROTO_TCP:
-            printf("   Protocol: TCP\n");
-            return;
-        case IPPROTO_UDP:
-            printf("   Protocol: UDP\n");
-            return;
-        case IPPROTO_ICMP:
-            printf("   Protocol: ICMP\n");
-            return;
-        default:
-            printf("   Protocol: others\n");
-            return;
-    }
+    struct tcpheader * tcp = (struct tcpheader *)
+                            ((u_char *)ip + (ip->iph_ihl*4));                 
+    printf("tcp    From: %d\n", ntohs(tcp->tcp_sport));   
+    printf("tcp      To: %d\n", ntohs(tcp->tcp_dport));           
   }
 }
 
@@ -62,7 +80,7 @@ int main()
   pcap_t *handle;
   char errbuf[PCAP_ERRBUF_SIZE];
   struct bpf_program fp;
-  char filter_exp[] = "icmp";
+  char filter_exp[] = "tcp";
   bpf_u_int32 net;
 
   // Step 1: Open live pcap session on NIC with name enp0s3
